31428ae7abfbbee4fc9e86207ad6b3931562ae91
Maicon Teixeira <maiconteixeira@MacBook-Air-de-Maicon.local>
2025-05-24 10:47:30 -0300

    fix: adiciona import GetServerSideProps

--- pages/briefing/[slug].tsx
+++ pages/briefing/[slug].tsx
@@ -1,3 +1,28 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import Head from 'next/head';
+
+const BriefingPage = ({
+  htmlContent,
+  nome_projeto
+}: {
+  htmlContent: string;
+  nome_projeto: string;
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{`${nome_projeto} | AiMore`}</title>
+      </Head>
+      <main
+        className="min-h-screen bg-black text-white p-8"
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+    </>
+  );
+};
+
+export default BriefingPage;
+
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
